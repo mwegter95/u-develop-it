d8bb7e13329a0b6581394adcde0ec2261fe41181
hitmaker <mwegter95@gmail.com>
2022-01-02 10:25:09 -0600

    candidate routes and party routes are now modularized, with server.js cleaned up

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const db = require('./db/connection')
-const inputCheck = require('./utils/inputCheck');
-
 const apiRoutes = require('./routes/apiRoutes/index'); // can leave this as /apiRoutes/') without index. Node automatically looks for index when requiring the directory
 
 const PORT = process.env.PORT || 3001;
@@ -11,63 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// use apiRoutes
 app.use('/api', apiRoutes);
-
-
-// get all the parties
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-// get a single party with id as an argument
-app.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, row) => {
-        if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-        }
-        res.json({
-        message: 'success',
-        data: row
-        });
-    });
-});
-
-// delete a party
-app.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: res.message });
-        // checks if anything was deleted
-      } else if (!result.affectedRows) {
-        res.json({
-          message: 'Party not found'
-        });
-      } else {
-        res.json({
-          message: 'deleted',
-          changes: result.affectedRows,
-          id: req.params.id
-        });
-      }
-    });
-  });
-
-
 
 
 // Default response for any other request (Not Found)
@@ -75,6 +18,11 @@ app.use((req, res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
